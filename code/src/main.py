0f8146680e4d5929d9327cefc53bcdfd3d6a26f2
josh.apfel <joshua.l.apfel@gmail.com>
2026-03-22 14:49:39 -0400

    updated for get feed data func

--- code/src/main.py
+++ code/src/main.py
@@ -1,8 +1,19 @@
 import requests
+import feedparser
 
 URL = "https://ir.thomsonreuters.com/rss/news-releases.xml?items=15"
 
-response = requests.get(url=URL)
-xml_data = response.content
+def get_feed_data(url):
+  try:
+    response = requests.get(url=url, timeout=10)
+    response.raise_for_status()
+    return feedparser.parse(url)
+  except requests.RequestException as e:
+    print(f"Error fetching feed: {e}")
+    return None
 
-print(xml_data)
+print(get_feed_data(URL))
+  
+  # for entry in feed_var.entries:
+  #   if entry.title == 'Thomson Reuters Files 2025 Annual Report':
+  #   print([k for k in entry])
