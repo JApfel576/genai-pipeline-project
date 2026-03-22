14c245fca5f8cd484d24d496402f626dc13e406d
josh.apfel <joshua.l.apfel@gmail.com>
2026-03-22 15:12:25 -0400

    commented keys in code

--- code/src/main.py
+++ code/src/main.py
@@ -1,9 +1,11 @@
 import requests
 import feedparser
 
-URL = "https://ir.thomsonreuters.com/rss/news-releases.xml?items=15"
+REUTERS_FINANCE_URL = "https://ir.thomsonreuters.com/rss/news-releases.xml?items=15"
 
-def get_feed_data(url):
+
+# Make request to feed url 
+def get_feed(url):
   try:
     response = requests.get(url=url, timeout=10)
     response.raise_for_status()
@@ -12,8 +14,13 @@ def get_feed_data(url):
     print(f"Error fetching feed: {e}")
     return None
 
-print(get_feed_data(URL))
-  
-  # for entry in feed_var.entries:
-  #   if entry.title == 'Thomson Reuters Files 2025 Annual Report':
-  #   print([k for k in entry])
+# ['bozo', 'entries', 'feed', 'headers', 'etag', 'updated', 'updated_parsed', 'href', 'status', 'encoding', 'version', 'namespaces']
+feed = get_feed(REUTERS_FINANCE_URL)
+print(feed['headers'])
+# feed['bozo'], feed['feed'], feed['headers']
+
+# entry keys 
+# ['title', 'title_detail', 'links', 'link', 'summary', 'summary_detail', 'published', 'published_parsed', 'authors', 'author', 'author_detail', 'id', 'guidislink']  
+# for entry in feed.entries:
+#     if entry.title == 'Thomson Reuters Files 2025 Annual Report':
+#       print([k for k in entry])
