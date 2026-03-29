e46ed7344b79fa97bafd326f1291f92e160d932c
josh.apfel <joshua.l.apfel@gmail.com>
2026-03-29 13:21:23 -0400

    untested event change detection and tested formatting of filename parameters

--- code/src/feedpoller.py
+++ code/src/feedpoller.py
@@ -1,5 +1,8 @@
 import requests
 import feedparser
+from datetime import datetime, timezone
+from pathlib import Path
+
 
 ITEM_CNT = 15 # item count stays the same
 REUTERS_FINANCE_URL = f"""
@@ -8,6 +11,8 @@ https://ir.thomsonreuters.com/rss/news-releases.xml?items={ITEM_CNT}
 HEADER_KEYS = ['etag','updated', 'updated_parsed', 'href']
 ENTRY_KEYS = ['title', 'summary', 'published', 'published_parsed', 'id'
               , 'link']
+OUT_PATH = '/code/output/'
+
 
 class FeedPoller():
   def __init__(self, url = REUTERS_FINANCE_URL):
@@ -15,6 +20,25 @@ class FeedPoller():
     self.url = url
     self.header_keys = HEADER_KEYS
     self.entry_keys = ENTRY_KEYS
+    self.out_path = OUT_PATH
+    # Store last seen values. Update to retrieve
+    self.last_etag = None 
+    self.last_updated = None
+    self.last_hash = None 
+  
+  # Check if feed has changed based on header
+  def has_changed(self, feed:dict) -> bool:
+    etag = feed.get('etag')
+    if etag != self.last_etag:
+      return True
+    updated = feed.get('updated')
+    if updated != self.last_updated:
+      return True
+    entries = feed.entries
+    hash = "".join(e.get('id') for e in entries)
+    if hash != self.last_hash:
+      return True
+    return False
 
   # Make request to feed url 
   def get_feed(self) -> dict | None:
@@ -47,5 +71,23 @@ class FeedPoller():
     feed = self.get_feed()
     header_dict = self.process_header(feed, self.header_keys)
     entry_dict = self.process_entries(feed, self.entry_keys)
-    merged_dict = {**header_dict, "items":entry_dict}  
-    return merged_dict
+    header_entries = {**header_dict, "items":entry_dict}  
+    return header_entries
+  
+  # Get current datetime and format
+  def get_now_utc(self) -> str:
+    now_utc = datetime.now(timezone.utc)\
+      .strftime('%Y%m%d_%H%M%S')
+    return now_utc
+  
+  def format_path(self):
+    cwd = Path.cwd()
+    cwd = str(cwd).replace('\\', '/').replace(':', '')
+    return cwd
+  
+  # Append changes to existing file or create new
+  def write_to_file(self, header_entries:dict):
+    cwd = self.format_path()
+    now_utc = self.get_now_utc()
+    filename = str(cwd) + self.out_path + now_utc
+    return filename
