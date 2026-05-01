df63777434e4b532e2809e808502c35def2e8ad5
josh.apfel <joshua.l.apfel@gmail.com>
2026-04-30 20:17:41 -0400

    read or provide url fastapi

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from enum import Enum
+
+
+class DefaultStr(str, Enum):
+  URL = "https://news.google.com/rss/searchq=site%3A%20reuters.com&hl=en-US&gl=US&ceid=US%3Aen" 
+
 
 app = FastAPI()
 
 @app.get("/health")
 def health_check():
   return {"status": "ok"}
+
+@app.get("/url")
+@app.get("/url/{url}")
+def url_provider(url: str | None = None):
+  return url or DefaultStr.URL.value
