ee197f729b71daf1d48185e1e160a97ed206df45
josh.apfel <joshua.l.apfel@gmail.com>
2026-05-02 11:49:32 -0400

    Updated fastapi url_provider method for google news site search verification

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from enum import Enum
+from typing import Annotated
+import re 
 
 
 class DefaultStr(str, Enum):
-  URL = "https://news.google.com/rss/searchq=site%3A%20reuters.com&hl=en-US&gl=US&ceid=US%3Aen" 
+  URL = "https://news.google.com/rss/search?q=site%3A%20reuters.com&hl=en-US&gl=US&ceid=US%3Aen" 
 
 
 app = FastAPI()
@@ -12,7 +14,15 @@ app = FastAPI()
 def health_check():
   return {"status": "ok"}
 
+# if url is not provided or without proper format return default
 @app.get("/url")
-@app.get("/url/{url}")
-def url_provider(url: str | None = None):
-  return url or DefaultStr.URL.value
+def url_provider(url_input: Annotated[str | None
+, Query(
+  description= "Google News RSS site: url"  
+)] = None
+):
+  pattern = r"^https?:\/\/news\.google\.com\/search\?q=site(\:|%3A)(?:%20|\s)?[a-z0-9.-]+\.com(&.*)?$"
+  url_input=str(url_input)
+  if url_input and re.match(pattern, url_input):
+    return url_input
+  return f"Malformed input...returning default url: {DefaultStr.URL.value}"
