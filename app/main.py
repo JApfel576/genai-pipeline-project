82af18c368c1c16b5237013dccdf9bc8cce06d89
josh.apfel <joshua.l.apfel@gmail.com>
2026-05-03 22:14:45 -0400

    added model for response data and cleaned up url creation

--- app/main.py
+++ app/main.py
@@ -1,26 +1,48 @@
 from fastapi import FastAPI, Query
 from enum import Enum
-from typing import Annotated
+from typing import Annotated, Any
 import re 
-from pydantic import AfterValidator
+from pydantic import BaseModel, AfterValidator
+import json
 
 
 class DefaultStr(str, Enum):
   URL = "https://news.google.com/search?q=site%3A%20bbc.com&hl=en-US&gl=US&ceid=US%3Aen" 
 
 
+class DefaultRePattern(str, Enum):
+  pattern = r"^https?:\/\/news\.google\.com\/search\?q=site(\:|%3A)(?:%20|\s)?[a-z0-9.-]+\.com(&.*)?$"
+
+
+class Header(BaseModel):
+    etag: str | None = None
+    updated: str
+
+
+class Item(BaseModel):
+    title: str
+    summary: str
+    published: str | None = None
+    guid: str | None = None
+    link: str
+
+
+class Model(BaseModel):
+    header: Header
+    items: list[Item]
+
 app = FastAPI()
 
 # Ensure url matches format for google news site search
 def check_url(url_input):
-  pattern = r"^https?:\/\/news\.google\.com\/search\?q=site(\:|%3A)(?:%20|\s)?[a-z0-9.-]+\.com(&.*)?$"
+  pattern = DefaultRePattern.pattern.value
   url_input=str(url_input)
   value_error_str = f"Malformed input...returning default url: {DefaultStr.URL.value}"
   if not url_input:
     raise ValueError(value_error_str)
   if not re.match(pattern, url_input):
     raise ValueError(value_error_str)
-  return True
+  return url_input
 
 # Create url from one given for rss feed
 def create_url(url_input):
@@ -42,9 +64,22 @@ def url_provider(url_input: Annotated[str | None
 , Query(
   description= "Url for google news search"
 ) ] = None
-):
-  if check_url(url_input):
-    return create_url(url_input)    
+) -> str:
+  try:
+     validated = check_url(url_input)
+     return create_url(validated) 
+  except ValueError as e:
+     return e
 
-
-print(url_provider(DefaultStr.URL.value))
+@app.get("/data", response_model=Model)
+def feed_data(url_input: Annotated[str | None, AfterValidator(check_url)] = None) -> Any:
+  data = {
+    "header":{"etag":""
+              , "updated":""}
+    , "items": [{"title":"test"
+              , "summary":""
+              , "published":""
+              , "guid":""
+              , "link":""}]
+              }
+  return data                                 
